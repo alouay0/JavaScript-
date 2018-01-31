2a0ec7323e0a57c85f520f782f5d848f5af4ac4b
alouay0 <34518933+alouay0@users.noreply.github.com>
2018-01-31 15:36:36 +0200

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+// Here the satrt of JavasScript
+
+// Declare a variable x and initialize it with an integer.
+
+let x= 24;
+console.log(x);
+
+
+// How do you round the number 7.25, to the nearest whole number?
+
+let x= 7.25;
+console.log(Math.round(x));
+
+//create a array called colors with the strings
+
+let colors= ['blue', 'red', 'green'];
+console.log(colors.length);
+
+let w = 'Gold';
+let n = 'Oil';
+
+if (typeof w === typeof n) {
+  console.log('SAME TYPE');
+}
+
+
+
+let x = 7;
+x = x % 3;
+console.log(x);
+
+// output 1
